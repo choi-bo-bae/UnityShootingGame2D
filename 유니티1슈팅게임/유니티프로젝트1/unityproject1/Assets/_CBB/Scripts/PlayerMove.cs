f6d7d85ab7bb3c87cdfbd09c4de7b1efb269afd4
qhqo727@gmai.com <qhqo727@gmail.com>
2020-06-01 12:01:34 +0900

    유니티 프로젝트1 _ 슈팅게임
    사용 버전 : 2019.3.13
    스트립트 : Visual Studio 2017

---
+++ 유니티1슈팅게임/유니티프로젝트1/unityproject1/Assets/_CBB/Scripts/PlayerMove.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    //플레이어 이동
+    public float speed = 5.0f;//플레이어의 이동 속도
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //플레이어 이동 함수
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+
+        transform.Translate(h * speed * Time.deltaTime, v * speed * Time.deltaTime, 0.0f);
+
+        //Vector3 dir = Vector3.right * h + Vector3.up * v;
+
+        //transform.Translate(dir * speed * Time.deltaTime);
+
+        //위치 = 현재 위치 + (방향 * 시간)
+
+        //Vector3 dir = new Vector3(h, v, 0);
+        //transform.position += dir * speed * Time.deltaTime;
+    }
+}

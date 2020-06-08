a9d1896bd90c29d4de87097769ad14ca0b0c5bc1
최보배 <qhqo727@gmail.com>
2020-06-08 13:57:47 +0900

    알파버전 모델 교체
    -백그라운드 스크롤링
    -캐릭터 모델 교체(플레이어, 애너미, 보스)

---
+++ 유니티1슈팅게임/유니티프로젝트1/unityproject1/Assets/_CBB/Scripts/Background.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+
+    public MeshRenderer back;
+
+    public float speed = 0.0f;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        back = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //백그라운드 스크롤
+        speed += 0.5f * Time.deltaTime;
+        Vector2 bgFlow = new Vector2(0, speed);
+        back.material.SetTextureOffset(1, bgFlow);
+
+        //스피드가 너무 쌓인다 싶으면 지우기
+        if(speed >= 5.0f)
+        {
+            speed = 0.0f;
+        }
+        //백그라운드 스크롤
+    }
+
+}
